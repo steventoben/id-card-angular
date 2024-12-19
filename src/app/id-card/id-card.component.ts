d37ab94db777d9387878f6633b2024cf7ea8ade0
Steven <steventoben@gmail.com>
2024-12-19 11:55:08 -0600

    added jost font and font-awesome

--- src/app/id-card/id-card.component.ts
+++ src/app/id-card/id-card.component.ts
@@ -1,18 +1,25 @@
 import {Component, Input, signal} from '@angular/core';
 import {IdCard} from './id-card.interface';
 import {IdCardDocumentComponent} from '../id-card-document/id-card-document.component';
+import {sampleIdCard} from './id-card';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-id-card',
   imports: [
-    IdCardDocumentComponent
+    IdCardDocumentComponent,
+    NgClass
   ],
   templateUrl: './id-card.component.html',
   styleUrl: './id-card.component.scss'
 })
 export class IdCardComponent {
-  @Input() idCard: IdCard;
+  @Input() idCard: IdCard = sampleIdCard;
   isEditing = signal(false);
   constructor() {
+  }
+
+  toggleEditing() {
+    this.isEditing.update(value => !value);
   }
 }
--- src/app/id-card/id-card.ts
+++ src/app/id-card/id-card.ts
@@ -2,7 +2,10 @@ import {IdCard} from './id-card.interface';
 
 export const sampleIdCard: IdCard = {
   dateOfBirth: '05/09/1998',
-  documents: [],
+  documents: [
+    {fileName: 'id-card.png', documentType: 'png'},
+    {fileName: 'Healthcare-info.doc', documentType: 'doc'},
+  ],
   email: 'EbonyMoore456@gmail.com',
   firstName: 'Ebony',
   homeAddress: '123 Maple Street, Cleveland, OH 44119',
